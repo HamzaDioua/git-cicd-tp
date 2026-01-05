b2be9712aaec9a4f208a016e3bc6a9537edaa453
Hamza Dioua <hamzadioua@gmail.com>
2026-01-06 00:26:51 +0100

    Update message in feature-greet branch

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print('Bonjour le monde')
 def greet(name):
-  print(f"Bonjour, {name}!")
+    return f"Hello, {name}!"
+
+def main():
+    print("Welcome to the feature-greet branch!")
+    print(greet("Hamza"))
+
+if __name__ == "__main__":
+    main()
